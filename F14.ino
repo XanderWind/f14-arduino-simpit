0d2231b04634b66d4e693a19066355134436c5af
Alexander Wind <wind2860@gmail.com>
2020-06-09 22:48:24 +0200

    Added master reset

--- F14.ino
+++ F14.ino
@@ -3,9 +3,11 @@
 #include <SwitchMatrix.h>
 #include <DcsBios.h>
 
+const int MASTER_RESET_PIN = 2;
 const int GEAR_DOWN_PIN = 7;
 const int GEAR_UP_PIN = 5;
 
+DcsBios::Switch2Pos pltMasterReset("PLT_MASTER_RESET", MASTER_RESET_PIN);
 DcsBios::RotaryEncoder pltGearLever("PLT_GEAR_LEVER", "DEC", "INC", GEAR_DOWN_PIN, GEAR_UP_PIN);
 
 void setup() {
