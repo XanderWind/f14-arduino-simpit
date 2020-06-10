c467a3d80253e283500ba141646995b16679c681
Alexander Wind <wind2860@gmail.com>
2020-06-10 21:56:53 +0200

    Added emergency stores jettison, nose strut (kneel and extend).

--- F14.ino
+++ F14.ino
@@ -3,10 +3,15 @@
 #include <SwitchMatrix.h>
 #include <DcsBios.h>
 
-const int MASTER_RESET_PIN = 2;
-const int GEAR_DOWN_PIN = 7;
-const int GEAR_UP_PIN = 5;
+const int NOSE_STRUT_KNEEL_PIN = 7;
+const int NOSE_STRUT_EXT_PIN = 6;
+const int EMERGENCY_STORES_JETTISON = 2;
+const int MASTER_RESET_PIN = 3;
+const int GEAR_DOWN_PIN = 5;
+const int GEAR_UP_PIN = 4;
 
+DcsBios::Switch3Pos pltNoseStrutSw("PLT_NOSE_STRUT_SW", NOSE_STRUT_EXT_PIN, NOSE_STRUT_KNEEL_PIN);
+DcsBios::Switch2Pos pltEmergStoreJett("PLT_EMERG_STORE_JETT", EMERGENCY_STORES_JETTISON);
 DcsBios::Switch2Pos pltMasterReset("PLT_MASTER_RESET", MASTER_RESET_PIN);
 DcsBios::RotaryEncoder pltGearLever("PLT_GEAR_LEVER", "DEC", "INC", GEAR_DOWN_PIN, GEAR_UP_PIN);
 
